4df4afd257776403c34bc975908c393e17f58d75
defined <defined@MacBook-Pro-donotyou.local>
2026-06-26 05:47:20 +0300

    init 4

--- src/assets/styles/entries/top.js
+++ src/assets/styles/entries/top.js
@@ -8,4 +8,8 @@ import '@/assets/styles/form/components/form-radio.css'
 import '@/assets/styles/ui/link-icon.css'
 import '@/assets/styles/ui/header/page-header.css'
 
+import '@/assets/styles/ui/pages/page-home.css'
+
 import '@/assets/styles/ui/sections/section-hero.css'
+import '@/assets/styles/ui/sections/section-quality.css'
+import '@/assets/styles/ui/sections/section-equipment.css'
