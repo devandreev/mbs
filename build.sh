ed15b0f2c64c2beab6b560a8f4170d94983bc663
defined <defined@MacBook-Pro-donotyou.local>
2026-06-29 21:58:30 +0300

    init 8

--- build.sh
+++ build.sh
@@ -11,23 +11,13 @@ cp ./public/favicon.ico ./public/dist/favicon.ico
 
 cd ./public/dist
 
-grep -rli '/images/' * | xargs -i@ sed -i 's/\/images\//\.\/images\//g' @
-grep -rli '.css"' * | xargs -i@ sed -i "s/\.css\"/\.css?v=$(timestamp)\"/g" @
-grep -rli '.js"' * | xargs -i@ sed -i "s/\.js\"/\.js?v=$(timestamp)\"/g" @
+grep -rli '/images/' * | xargs -I@ sed -i '' 's/\/images\//\.\/images\//g' @
+grep -rli '.css"' * | xargs -I@ sed -i '' "s/\.css\"/\.css?v=${timestamp}\"/g" @
+grep -rli '.js"' * | xargs -I@ sed -i '' "s/\.js\"/\.js?v=${timestamp}\"/g" @
 
-cd assets/js
-
-# Удаление переносов строк в шаблонных литералах
-cat base.bundle.js | tr -d '\n' > temp.bundle.js
-mv temp.bundle.js base.bundle.js
-
-rm styles.bundle.js
-
-cd ../../
-
-# git init
-# git add --all
-# git commit -m "build"
-# git branch -M dist
-# git remote add origin git@github.com:username/repo.git
-# git push -u origin dist --force
+ git init
+ git add --all
+ git commit -m "build"
+ git branch -M dist
+ git remote add origin git@github.com:devandreev/mbs.git
+ git push -u origin dist --force
