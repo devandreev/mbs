16c2cf40471aba5c324b5b01397dfb51e3bd586d
defined <defined@MacBook-Pro-donotyou.local>
2026-07-01 07:24:37 +0300

    init 18

--- webpack.config.js
+++ webpack.config.js
@@ -198,13 +198,6 @@ module.exports = (env, options) => {
           test: /\.svg/,
           exclude: /(fonts)/,
           type: 'asset/inline',
-        },
-        {
-          test: /\.(png|jpe?g|gif|webp)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'images/[name][ext]'
-          }
         }
       ]
     },
